ea0333889e35a65235646186c8ea3a6aa6cc7315
Lorenzo Llamas <ltl924@gmail.com>
2019-04-18 00:40:01 -0400

    everything is working i think

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,8 @@
 require_relative '../config/environment'
 require 'tty-prompt'
 #pputs "Hi, Welcome to my game"
-
+#Group.where(team_id: team_id).map{|group| puts Character.find_by(id: group.character_id).char_name}
+#^finding all characters in a specific team through group
 $character_list = ["Captain America", "Black Panther", "Doctor Strange", "Scarlett Witch", "Thor", "Loki", "Iron Man", "Spider-Man", "Captain Marvel", "Hawkeye"]
 
 def create_group(char_id, team_id)
@@ -13,7 +14,7 @@ def create_team
 	thanos = Character.find_by(char_name: "Thanos")
 	team_array = []
 	my_team_atk = 0
-	puts "Please enter your team name that will fight #{thanos.char_name}"
+	puts "Please enter your team name that will fight #{thanos.char_name} of 1000 HP"
 	my_team_name = gets.chomp
 	my_team = Team.create(team_name: my_team_name, team_atk: 0)
 
@@ -113,9 +114,11 @@ def change_char(character_name,team_id,group_id,available_chars)
 end
 
 
-def view_single_team(single_team)
+def view_single_team(single_team_string)
 	# available_chars = $character_list
+	single_team = single_team_string.split(',')[0]
 	available_chars = []
+	# binding.pry
 	single_team_id = Team.find_by(team_name: single_team).id
 	single_group = Group.where(team_id: single_team_id)
 	chars_in_team_instances = []
@@ -128,14 +131,19 @@ def view_single_team(single_team)
 		char_inst[0].char_name 
 	end
 	
-
+	char_names_in_team << "-Fight"
 	char_names_in_team << "-Back"
+
 	char_to_change = $prompt.select("Substitute Character ?", [char_names_in_team])
 	# binding.pry
 	if char_to_change == "-Back"
 		system "clear"
 		view_teams
+	elsif char_to_change == "-Fight"
+		system "clear"
+		fight(single_team_id)
 	else
+		char_names_in_team.delete("-Fight")
 		char_names_in_team.delete("-Back")
 		$character_list.map do |charname|
 			if char_names_in_team.include?(charname)
@@ -145,22 +153,10 @@ def view_single_team(single_team)
 			end
 
 		end
-		# char_names_in_team.each do |charname|
-		# 	if $character_list.include?(charname) 
-		# 	# binding.pry
-		# 		available_chars.delete(charname)
-		# 	end
-		# end
-		# binding.pry
+		
 		change_char(char_to_change,single_group[0].team_id,single_group[0].id,available_chars)
 	end
-	# if yn == "Yes"
-		# char_to_change = $prompt.select("Substitute Character ?", [chars_in_team_instances.map do |char_inst| char_inst[0].char_name end])
-	# elsif yn == "No"
-	# 	view_teams
-	# end
 	
-	#p Character.find_by(id: single_group.character_id)
 end
 
 def view_teams
@@ -170,7 +166,11 @@ def view_teams
 		team_lst << Team.find_by(id: group.team_id)
 	end
 	# team_lst.uniq
-	team_names = team_lst.uniq.map{|team| team.team_name}
+	# team_names_hash = {}
+
+	team_names = team_lst.uniq.map do |team| 
+		"#{team.team_name}, ATK: #{team.team_atk}"
+	end
 	team_names << "-Back"
 
 	single_team = $prompt.select("Teams:", [team_names])
@@ -203,9 +203,12 @@ def team_selection
 	elsif team == "View Teams"
 		view_teams
 		team
-	else
+	elsif team == "Delete A Team"
 		delete_team
 		team
+	# elsif team == "Attack Thanos"
+	# 	attack
+	# 	team
 	end
 
 
@@ -229,10 +232,59 @@ def delete_team
 		deleted_team_id = Team.find_by(team_name: team_name_to_delete)
 		Group.where(team_id: deleted_team_id).destroy_all
 		puts "Deleted #{Team.find_by(team_name: team_name_to_delete).team_name}"
+		Team.where(id: deleted_team_id).destroy_all
+		
 		# binding.pry
 		team_selection
 	end
 end
+
+def fight(team_id)
+	chars_in_current_team_fighting = Group.where(team_id: team_id).map{|group| Character.find_by(id: group.character_id).char_name}
+	thanos = Character.find_by(char_name: "Thanos")
+	current_team = Team.find_by(id: team_id)
+	puts "#{current_team.team_name} challenges Thanos with the following Avengers:"
+	# p chars_in_current_team_fighting
+
+	chars_in_current_team_fighting.each do |charname|
+		if charname == Character.find_by(char_name: charname).char_name
+				p "#{charname}, ATK: #{Character.find_by(char_name: charname).char_atk}"
+				# team_to_change.team_atk += Character.find_by(char_name: charname).char_atk
+				# binding.pry
+		end
+	end
+
+	bonus = ""
+
+	if chars_in_current_team_fighting.include?("Captain America") && chars_in_current_team_fighting.include?("Iron Man")
+		current_team.team_atk += 275
+		bonus = "CA+IM"
+	end
+	if chars_in_current_team_fighting.include?("Scarlett Witch") && chars_in_current_team_fighting.include?("Doctor Strange") && chars_in_current_team_fighting.include?("Iron Man")
+		current_team.team_atk += 1000
+		bonus = "SW+DS+IM"
+	end
+	if chars_in_current_team_fighting.include?("Thor") && chars_in_current_team_fighting.include?("Captain Marvel")
+		current_team.team_atk += 875
+		bonus = "T+CM"
+		# binding.pry
+	end
+	puts "===================================================="
+	sleep(1)
+	# binding.pry
+	if current_team.team_atk > thanos.char_atk
+		puts "#{current_team.team_name} is victorious against Thanos!"
+		puts "Thanks to the #{bonus} combination, #{current_team.team_name} increased their atk powers to #{current_team.team_atk}\n\n"
+		puts "===================================================="
+	else
+		puts "#{current_team.team_name} of #{current_team.team_atk} ATK has been defeated by Thanos!"
+		puts "Maybe try a different combination of heroes to challenge the Mad Titan\n\n"
+		puts "===================================================="
+	end
+		team_selection
+	# binding.pry
+end
+	
 
 
 welcome
